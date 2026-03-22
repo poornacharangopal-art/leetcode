b2d4d4a57fd688ccaff9420435bd8c30c6c52efe
Poornacharangopal <poornacharangopal@gmail.com>
2026-03-22 18:20:14 +0530

    Time: 0 ms (100%), Space: 22.5 MB (78.49%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        reverse(citations.begin(),citations.end());
+        if(citations.size()==1&&citations[0]>=1){
+            return 1;
+        }
+        for(int i=0;i<citations.size();i++){
+            if(!(citations[i]>=i+1)){
+                return i;
+            }
+        }
+        return citations.size();
+    }
+};
