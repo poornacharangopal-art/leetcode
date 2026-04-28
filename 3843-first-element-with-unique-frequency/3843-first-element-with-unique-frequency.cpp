7eac1abda1fc15344a63db73dae2c24fa9fbb7bb
Poornacharangopal <poornacharangopal@gmail.com>
2026-04-28 13:51:21 +0530

    Time: 187 ms (36.9%), Space: 289.5 MB (93.95%) - LeetHub

--- 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.cpp
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
     int firstUniqueFreq(vector<int>& nums) {
-        unordered_map<int,int>firstIndex;
         unordered_map<int,int>mp1;
         unordered_map<int,int>mp2;
-        int i=0;
-        for(int x:nums){
-            if(!firstIndex.count(x)){
-                firstIndex[x]=i;
-            }
-            i++;
-        }
         for(int x:nums){
             mp1[x]++;
         }
         for(auto&p:mp1){
             mp2[p.second]++;
         }
-        vector<int>v;
-        for(auto&p:mp1){
-            if(mp2[p.second]==1){
-                v.push_back(p.first);
+        for(int i=0;i<nums.size();i++){
+            if(mp2[mp1[nums[i]]]==1){
+                return nums[i];
             }
         }
-        if(v.empty())
         return -1;
-        else{
-            sort(v.begin(),v.end(),[&](int a,int b){
-                return firstIndex[a]<firstIndex[b];
-            });
-            return v[0];
-        }
     }
 };
