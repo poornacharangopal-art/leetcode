3d63fd190958d9bbff9db01b3eb849ed6ef68491
Poornacharangopal <poornacharangopal@gmail.com>
2026-05-03 09:46:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (98.19%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int countSegments(string s) {
+        if(s.size()==0||s==" "){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i] != ' ' && (i == 0 || s[i-1] == ' ')){
+                count++;
+            }
+        }
+        return count;
+    }
+};
