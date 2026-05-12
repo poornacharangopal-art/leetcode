bf5aba0914eb477a28c56983c5e5c3010a1134ec
Poornacharangopal <poornacharangopal@gmail.com>
2026-05-12 09:14:22 +0530

    Time: 0 ms (100%), Space: 95.5 MB (37.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,14 +4,15 @@ public:
         for(int i=0;i<nums2.size();i++){
             nums1.push_back(nums2[i]);
         } 
+        sort(nums1.begin(),nums1.end());
         double n;
         if(nums1.size()%2!=0){
-            nth_element(nums1.begin(),nums1.begin()+(nums1.size()-1)/2,nums1.end());
             n=nums1[(nums1.size()-1)/2];
         }
         else{
-            sort(nums1.begin(),nums1.end());
-            n=(nums1[(nums1.size()/2-1)]+nums1[(nums1.size()/2)])/2.0;
+            int l=nums1[(nums1.size())/2];
+             int m=nums1[(nums1.size())/2-1];
+             n=(l+m)/2.0;
         }
         return n;
     }
