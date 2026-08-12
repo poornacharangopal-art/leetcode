8be13001451948cdada815c319bb4358e47f8f84
Poornacharangopal <poornacharangopal@gmail.com>
2026-08-12 20:18:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.92%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int sum=0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0)
+        if(num==1||num==0)return false;
+        for(int i=1;i<=sqrt(num);i++){
+            if(num%i==0){
             sum+=i;
-        }
+            if (i!=1&&i != num / i)
+                    sum += num / i;
+            }
+            }
         if(sum==num)return true;
         return false;
     }
