c235de3b7b24ee2d5f3208af923200642791dcaa
Poornacharangopal <poornacharangopal@gmail.com>
2026-08-12 11:24:09 +0530

    Time: 17 ms (79.28%), Space: 8.5 MB (100%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -3,7 +3,7 @@ public:
     bool isprime(int n){
         if(n==2)return true;
         if(n==1)return false;
-        for(int i=2;i<=n/2;i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0)
             return false;
         }
