cd51765ab9c2e9b239f10d8743e9b36d1a3a82d2
Poornacharangopal <poornacharangopal@gmail.com>
2026-08-19 21:08:18 +0530

    Time: 3 ms (50.36%), Space: 17.9 MB (69.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int count=1,index,min=0;
-        vector<int> p(k,0);
-        int j=0;
-        while(j<k){
-            for(int i=0;i<nums.size();i++){
-                int l=i+1;
-                while(l<nums.size()){
-                    if(nums[i]!=nums[l]){
-                        break;
-                    }
-                    else{
-                        count+=1;
-                        l++;
-                    }
-                }
-                if(count>min){
-                    min=count;
-                    index=i;
-                }
-                count=1;
-                i=l-1;
-            }
-            p[j]=nums[index];
-            nums.erase(nums.begin()+index,nums.begin()+index+min);
-            count=1;
-            min=0;
-            j++;
+        unordered_map<int,int>mp;
+        for(int num:nums){
+            mp[num]++;
         }
-        return p;
+        vector<int>num;
+        for(auto&p:mp){
+            num.push_back(p.first);
+        }
+        sort(num.begin(),num.end(),[&](int a,int b){
+            if(mp[a]==mp[b]){
+                return a<b;
+            }
+            return mp[a]>mp[b];
+        });
+        vector<int>ans(num.begin(),num.begin()+k);
+        return ans;
     }
 };
