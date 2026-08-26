74f0119913f816b701e212817062bd21ef719a18
Poornacharangopal <poornacharangopal@gmail.com>
2026-08-26 11:40:11 +0530

    Time: 0 ms (100%), Space: 42 MB (41.93%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int i=0,j=0;
-        int count=0;
         int sum=0;
-        int min=INT_MAX;
+        int mini=INT_MAX;
         while(j<nums.size()){
             sum+=nums[j];
             if(sum>=target){
-                if(j-i+1<min){
-                    min=j-i+1;
+                mini=min(mini,j-i+1);
+                while(i<j&&sum>=target){
+                    sum-=nums[i];
+                    i++;
+                    if(sum>=target){
+                         mini=min(mini,j-i+1);
+                    }
                 }
-                i++;
-                j=i;
-                sum=0;
-                count=0;
-                continue;
             }
             j++;
         }
-        if(min==INT_MAX){
+        if(mini==INT_MAX){
             return 0;
         }
-        return min;
+        return mini;
     }
 };
