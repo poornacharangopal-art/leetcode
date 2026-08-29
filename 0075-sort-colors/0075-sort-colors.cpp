d0c646056f372c29cc324142472bb650f1441c51
Poornacharangopal <poornacharangopal@gmail.com>
2026-08-29 12:42:19 +0530

    Time: 0 ms (100%), Space: 11.5 MB (98.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int a=0,b=1,c=2;
-        int low=0,mid=0,high=nums.size()-1;
+    
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
         while(mid<=high){
-            if(nums[mid]==a){
-                swap(nums[mid],nums[low]);
+            int n=nums[mid];
+            if(n==0){
+                swap(nums[low],nums[mid]);
                 low++;
                 mid++;
             }
-            else if(nums[mid]==b){
+            else if(n==1){
                 mid++;
             }
             else{
