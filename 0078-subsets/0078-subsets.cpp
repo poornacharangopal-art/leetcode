5d40ef87ff883ebfa82c15cd4fc7550cdad68fcf
Poornacharangopal <poornacharangopal@gmail.com>
2026-09-07 20:30:14 +0530

    Time: 0 ms (100%), Space: 10 MB (59.49%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-void subset(vector<int>& nums,vector<vector<int>>&ans,int i,vector<int>&v){
-    i++;
-    while(i<nums.size()){
-        v.push_back(nums[i]);
-        ans.push_back(v);
-        subset(nums,ans,i,v);
-        v.pop_back();
-        int curr=nums[i];
-        while(i<nums.size()&&nums[i]==curr){
-            i++;
-        }
-        i--;
-        i++;
-    }
-   }
-    vector<vector<int>> subsets(vector<int>& nums) {
-           sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        vector<int>p;
-        ans.push_back(p);
-        int i=0;
-        while(i<nums.size()){
-            vector<int>v;
-            v.push_back(nums[i]);
+    void backtrack(int ind,vector<int>&nums,vector<int>&v,vector<vector<int>>&ans){
+        if(ind==nums.size()){
             ans.push_back(v);
-            subset(nums,ans,i,v);
-            while(i<nums.size()-1&&nums[i]==nums[i+1]){
-                i++;
-            }
-            i++;
+            return;
         }
+        v.push_back(nums[ind]);
+        backtrack(ind+1,nums,v,ans);
+        v.pop_back();
+        backtrack(ind+1,nums,v,ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>v;
+        vector<vector<int>>ans;
+        backtrack(0,nums,v,ans);
         return ans;
     }
 };
