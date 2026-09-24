4bf7aa3b2622f851000a2e5c5661d4f7d739365e
Poornacharangopal <poornacharangopal@gmail.com>
2026-09-25 05:11:13 +0530

    Time: 470 ms (89.04%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,7 +2,8 @@
 SELECT id, CASE 
 WHEN p_id IS NULL THEN "Root"
 WHEN id IN (SELECT DISTINCT p_id
-FROM Tree)THEN "Inner"
+FROM Tree
+WHERE p_id IS NOT NULL)THEN "Inner"
 ELSE "Leaf"
 END AS type
 FROM Tree;
